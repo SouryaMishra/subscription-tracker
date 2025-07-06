355a69435e92f0751857b80878796e69ba6fb86c
Sourya Mishra <sourya.mishra111@gmail.com>
2025-07-06 22:45:42 +0530

    setup express and environment variables

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import { PORT } from "./config/env.js";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.get("/", (req, res) => {
+  res.send("Welcome to Subscription Tracker API!");
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API is listening on http://localhost:${PORT}`);
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+// export default app;
