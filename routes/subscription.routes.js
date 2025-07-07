e49e9da0d99826fcf329a0c580fed5f16dbbf395
Sourya Mishra <sourya.mishra111@gmail.com>
2025-07-07 23:17:49 +0530

    implement creating and fetching subscriptions

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get("/", (req, res) => res.send({ title: "GET all subscriptions" }));
+subscriptionRouter.get("/", authorize, getUserSubscriptions);
 
 subscriptionRouter.get("/:id", (req, res) => res.send({ title: "GET subscription details" }));
 
-subscriptionRouter.post("/", (req, res) => res.send({ title: "CREATE subscription" }));
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) => res.send({ title: "UPDATE subscription" }));
 
