67f150709d8ae0af288ba9998e466ddb29d0a7b9
Sourya Mishra <sourya.mishra111@gmail.com>
2025-07-08 22:02:35 +0530

    create subscription workflow with upstash

--- config/env.js
+++ config/env.js
@@ -2,4 +2,15 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, DB_URI, NODE_ENV, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_KEY } = process.env;
+export const {
+  PORT,
+  DB_URI,
+  NODE_ENV,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_KEY,
+  QSTASH_TOKEN,
+  QSTASH_URL,
+  SERVER_URL,
+  EMAIL_PASSWORD,
+} = process.env;
