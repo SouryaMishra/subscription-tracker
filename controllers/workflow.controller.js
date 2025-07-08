45fdf057d1ffc9de41df14a198a6ee48b603905d
Sourya Mishra <sourya.mishra111@gmail.com>
2025-07-08 22:52:45 +0530

    send reminder mail with nodemailer

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -3,6 +3,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
 import Subscription from "../models/subscription.model.js";
+import { sendReminderEmail } from "../utils/send-email.js";
 
 const REMINDERS = [7, 5, 2, 1];
 
@@ -27,7 +28,11 @@ export const sendReminders = serve(async (context) => {
     }
 
     if (dayjs().isSame(reminderDate, "day")) {
-      await triggerReminder(context, `${daysBefore} days before reminder`, subscription);
+      await triggerReminder(
+        context,
+        `${daysBefore} ${daysBefore === 1 ? "day" : "days"} before reminder`,
+        subscription
+      );
     }
   }
 });
@@ -46,5 +51,10 @@ const sleepUntilReminder = async (context, label, date) => {
 const triggerReminder = async (context, label, subscription) => {
   return await context.run(label, async () => {
     console.log(`Triggering ${label} reminder`);
+    await sendReminderEmail({
+      to: subscription.user.email,
+      type: label,
+      subscription,
+    });
   });
 };
